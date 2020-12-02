0c330d715a9e3f6ccc0169d3328b0f2f6964c871
adi <singhadi672@gmail.com>
2020-12-02 22:45:54 +0530

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+let button = document.querySelector("#btn-translate");
+
+button.addEventListener("click",()=>{
+    let choice = document.querySelector("#select-language").value;
+    console.log(choice)});

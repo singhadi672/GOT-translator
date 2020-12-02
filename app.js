c647f2ac1fe03f0ff13932000d5b6305b981eaa2
adi <singhadi672@gmail.com>
2020-12-02 23:10:31 +0530

    feat: added js file

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-
+let selectedLang = document.querySelector("#select-language");
 let button = document.querySelector("#btn-translate");
+let inputText = document.querySelector("#txt-input");
+let outputText = document.querySelector("#txt-translated");
+
+function errorHandler(error){
+    console.log("error is " + error);
+    alert("there is an error....please try after some time!!");
+}
 
 button.addEventListener("click",()=>{
-    let choice = document.querySelector("#select-language").value;
-    console.log(choice)});
+    let url = "https://api.funtranslations.com/translate/";
+    let language = selectedLang.value;
+    if(language==="Valyrian"){
+        url = url + "valyrian.json" +"?text=" + inputText.value;
+    }
+    else if(language==="Dothraki")
+    {
+        url = url + "dothraki.json" +"?text=" + inputText.value;
+    }
+    else{
+        url = url + "hodor.json" +"?text=" + inputText.value;
+    }
+
+    fetch(url).then(response => response.json())
+    .then(json => outputText.innerText = json.contents.translated)
+    .catch(errorHandler);
+})
